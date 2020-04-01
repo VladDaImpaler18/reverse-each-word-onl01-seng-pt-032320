15b4e0a74cd93bd1f6e3fe999915cf43dd98702e
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-01 02:52:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence)
   piecesOfSentence=sentence.split(" ")
   reversedSentence = piecesOfSentence.each { |word| reversedSentence << word.reverse}
+  reversedSentence
 end
